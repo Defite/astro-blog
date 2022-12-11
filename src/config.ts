7d9e36c31e3972340deb37ee5e9bc5bfadd2c36d
Defite <me@defite.ru>
2022-12-11 12:49:47 +0300

    feat: add post page

---
+++ src/config.ts
@@ -0,0 +1,9 @@
+export const SITE = {
+  website: "https://nikita.codes/",
+  author: "Nikita Makhov",
+  desc: "Personal notes about Volvo, frontend and games",
+  title: "Nikita Codes",
+  ogImage: "share.png",
+  lightAndDarkMode: true,
+  postPerPage: 3,
+};
