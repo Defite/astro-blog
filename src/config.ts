e8926e18a766458ed0aeff08403d9b218ff3b9ea
Defite <me@defite.ru>
2024-03-10 03:20:10 +0300

    feat: get settings from ghost

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,3 @@
 export const SITE = {
-  website: "https://nikita.codes/",
-  author: "Nikita Makhov",
-  desc: "Personal notes about Volvo, frontend and games",
   title: "Nikita Codes",
-  ogImage: "share.png",
-  lightAndDarkMode: true,
-  postPerPage: 5,
 };
